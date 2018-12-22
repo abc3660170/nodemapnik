620ee387b7b5e25421f517dd7ce201f299ef743d
abc3660170 <3413218@qq.com>
2018-12-22 15:34:41 +0800

    add init

---
+++ tileserver/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var server = require('./server/server.js');
+
+var app = express();
+app.all("*",function(req,res,next){
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow","X-Requested-With");
+	res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+	res.header("X-Powered-By",'3.2.1');
+	//res.header("Content-Type","application/json;charset=utf-8");
+	next();
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/t', server);
+
+var server_dark = require('./server/server_dark.js');
+app.use('/tsd', server_dark);
+
+var server_dark_transparent = require('./server/server_dark_transparent.js');
+app.use('/tsdt', server_dark_transparent);
+
+app.get('/', function(req, res){
+  res.sendfile('./public/index.html');
+});
+
+app.listen(3000);
---
+++ tileserver/server/server.js
@@ -0,0 +1,21 @@
+var tilestrata = require('tilestrata');
+var disk = require('tilestrata-disk');
+var mapnik = require('tilestrata-mapnik');
+var express = require('express');
+
+var strata = tilestrata();
+var router = express.Router();
+
+//layer名称不能为空
+strata.layer('map')
+  .route('tile.png')    //route方法中不能使用正则表达式
+  .use(disk.cache({dir: '/home/tilecache/style/'}))    //设置瓦片缓存在当前目录的tilecache之目录中
+  .use(mapnik({
+ 	 pathname: "/home/openstreetmap-carto/mapnik.xml"
+  }));
+
+router.use(tilestrata.middleware({
+  server: strata
+}));
+
+module.exports = router;
